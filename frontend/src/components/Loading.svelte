c7dbe2d6c902779ce8a3cf0df81a475557d0f8d9
Northeast-webdev <webdev.northeast@gmail.com>
2024-09-13 10:09:53 +0200

    changes to loader

--- frontend/src/components/Loading.svelte
+++ frontend/src/components/Loading.svelte
@@ -1,50 +1,34 @@
 <!-- components/Loading.svelte -->
 <script>
   export let iconOnly = false;
+  import logo from "../assets/logo.png";
 </script>
 
 {#if iconOnly}
 <div class="loader"></div>
 {:else}
-<main>
+<main class="fixed w-full min-h-screen flex items-center justify-center bg-gradient-to-tl from-green-700 to-green-800">
+  <div class="flex items-center justify-center gap-4 flex-col">
+  <img src={logo} alt="logo" class="w-10 h-10 object-contain" />
   <div class="loader"></div>
+  </div>
 </main>
 {/if}
 
 <style scoped>
 /* HTML: <div class="loader"></div> */
 .loader {
-  display: inline-flex;
-  gap: 5px;
+  height: 4px;
+  width: 130px;
+  @apply text-green-600;
+  --c:no-repeat linear-gradient(#fff6 0 0);
+  background: var(--c),var(--c),currentColor;
+  background-size: 60% 100%;
+  animation: l16 3s infinite;
 }
-.loader:before,
-.loader:after {
-  content: "";
-  width: 30px;
-  aspect-ratio: 1;
-  box-shadow: 0 0 0 5px inset #222;
-  animation: l4 1.5s infinite;
-}
-.loader:after {
-  --s: -1;
-  animation-delay: 0.75s
-}
-@keyframes l4 {
-  0%     {transform:scaleX(var(--s,1)) translate(0) rotate(0)}
-  16.67% {transform:scaleX(var(--s,1)) translate(-50%) rotate(0)}
-  33.33% {transform:scaleX(var(--s,1)) translate(-50%) rotate(90deg)}
-  50%,
-  100%   {transform:scaleX(var(--s,1)) translate(0) rotate(90deg)}
-}
-main {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-    background-color: #fff;
-    position: fixed;
-    width: 100%;
-    inset: 0;
-    z-index: 1000;
+@keyframes l16 {
+  0%   {background-position:-150% 0,-150% 0}
+  66%  {background-position: 250% 0,-150% 0}
+  100% {background-position: 250% 0, 250% 0}
 }
   </style>
