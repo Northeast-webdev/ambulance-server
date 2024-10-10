8f2af8e847cbc17a4d0e12228c7f1252458110b1
Northeast-webdev <webdev.northeast@gmail.com>
2024-10-10 16:44:49 +0200

    quick backend switch

--- backend/src/controllers/runController.js
+++ backend/src/controllers/runController.js
@@ -26,7 +26,7 @@ const createRun = async (request, reply) => {
       );
       run.patient = patient[0]._id;
     } else {
-      const words = pat.split(" ");
+      let words = pat.split(" ");
 
       for (let i = 0; i < words.length; i++) {
         words[i] = words[i][0].toUpperCase() + words[i].substr(1).toLowerCase();
