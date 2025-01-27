5bf6118befafeea2816d1423b4d932451a98bb13
Northeast-webdev <webdev.northeast@gmail.com>
2025-01-27 14:15:47 +0100

    added photos fix 2

--- backend/src/controllers/pdfController.js
+++ backend/src/controllers/pdfController.js
@@ -111,14 +111,12 @@ const printCarChecklist = async (request) => {
     return `
       <div style="display: flex; justify-content: center; gap: 20px; margin-bottom: 20px; flex-wrap: wrap; margin-top: 30px">
         ${photos.map((item) => `
-        <div style="width: 375px; margin-bottom: 20px;">
+        <div style="width: 250px; margin-bottom: 20px;">
           <img
             src="${item}" 
             style="
               width: 100%;
-              height: 100%;
-              aspect-ratio: 1;
-              object-fit: contain;
+              height: auto;
             "
           />
         </div>
