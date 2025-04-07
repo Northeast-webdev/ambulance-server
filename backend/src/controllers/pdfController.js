1935e019c5f54fd89bb6b333a500a83ed3c32101
Northeast-webdev <webdev.northeast@gmail.com>
2025-04-07 16:14:36 +0200

    cvbcvbb

--- backend/src/controllers/pdfController.js
+++ backend/src/controllers/pdfController.js
@@ -112,7 +112,7 @@ const printCarChecklist = async (request) => {
       <div style="display: flex; justify-content: center; gap: 20px; margin-bottom: 20px; flex-wrap: wrap; margin-top: 30px">
         ${photos.map(
           (item) => `
-        <div style="width: 250px; margin-bottom: 20px;">
+        <div style="width: 200px; margin-bottom: 20px;">
           <img
             src="${item}" 
             style="
