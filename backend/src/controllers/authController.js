a9cfa0baa1f10b799a7d0a34b8a080586d02ab0c
Northeast-webdev <webdev.northeast@gmail.com>
2025-04-14 18:29:48 +0200

    added err log to auth

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -62,6 +62,7 @@ const login = async (request, reply) => {
     const token = generateToken(user);
     reply.send({ token, id: user._id, role: user.role });
   } catch (err) {
+    console.log(err);
     reply.code(500).send({ error: err });
   }
 };
