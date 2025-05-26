9cf6e0b9cf8331a27f8737ae5dd023bae22cd2f8
Northeast-webdev <webdev.northeast@gmail.com>
2025-05-26 17:46:44 +0200

    Update sorting order in carChecklistController and materialChecklistController to ascending by creation date
    - Changed the sorting order in both controllers from descending to ascending based on the created_at field, ensuring that checklists are listed from oldest to newest.

--- backend/src/controllers/carChecklistController.js
+++ backend/src/controllers/carChecklistController.js
@@ -99,7 +99,7 @@ const listCarChecklists = async (request, reply) => {
       .populate("user", "first_name last_name _id")
       .skip((page - 1) * limit)
       .limit(limit)
-      .sort({ created_at: -1 })
+      .sort({ created_at: 1 })
       .exec();
 
     const total = await CarChecklist.countDocuments(query);
